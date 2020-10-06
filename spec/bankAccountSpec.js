2a8eda1258bfa4fa8cc190bd98649b7dc69aaf24
Yasmin Underdown <yunderdown1@gmail.com>
2020-10-06 12:01:51 +0100

    Withdraw method tests failing

--- spec/bankAccountSpec.js
+++ spec/bankAccountSpec.js
@@ -17,6 +17,14 @@ describe('BankAccount', function(){
         expect(account.balance).toEqual(1000)
         expect(account.transactions.length).toEqual(1)
         expect(account.printStatement()).toContain("|| 1000.00 || || 1000.00")
-    })
+    });
+
+    it('subtracts amount from balance when withdrawal occurs and is shown on statement', function(){
+        account.balance = 1000;
+        account.withdraw(250);
+        expect(account.balance).toEqual(750)
+        expect(account.transactions.length).toEqual(1)
+        expect(account.printStatement()).toContain("|| || 250.00 || 750.00")
+    });
 
 });
