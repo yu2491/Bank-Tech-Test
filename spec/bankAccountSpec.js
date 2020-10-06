d5cfe467a5947b1fcbb3de9c85fb1c163cf24fc1
Yasmin Underdown <yunderdown1@gmail.com>
2020-10-06 14:25:47 +0100

    Date to be included in statement tests failing

--- spec/bankAccountSpec.js
+++ spec/bankAccountSpec.js
@@ -3,9 +3,12 @@
 describe('BankAccount', function(){
 
     var account;
+    var dateString;
     
     beforeEach(function(){
         account = new BankAccount();
+        var date = new Date();
+        dateString = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
     });
 
     it('prints blank statement with date credit debit and amount headers if no transactions have been made', function(){
@@ -36,6 +39,17 @@ describe('BankAccount', function(){
         expect(account.transactions.length).toEqual(0)
         expect(account.printStatement()).toEqual("date || credit || debit || balance")
         
+    });
+
+    it('statement shows date for deposit', function() {
+        account.deposit(1000);
+        expect(account.printStatement()).toContain(`${dateString} || 1000.00 || || 1000.00`)
+    });
+
+    it('statement shows date for withdrawal', function() {
+        account.balance = 1000;
+        account.withdraw(250);
+        expect(account.printStatement()).toContain(`${dateString} || || 250.00 || 750.00`)
     });
 
 });
