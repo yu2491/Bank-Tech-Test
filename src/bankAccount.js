decffa58829f5f336be33b3abe48823514fffcf2
Yasmin Underdown <yunderdown1@gmail.com>
2020-10-06 10:48:52 +0100

    Deposit method tests passing

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -2,7 +2,20 @@
 
 class BankAccount {
 
+    constructor() {
+        this.balance = 0
+        this.transactions =  []
+    }
+
+    deposit(amount) {
+        this.balance += amount
+        this.transactions.push(`|| ${amount} || || ${this.balance}`)
+    }
+
     printStatement() {
+        if(this.transactions.length > 0) {
+            return "date || credit || debit || balance" + this.transactions[0];
+        }
         return "date || credit || debit || balance"
     }
 
