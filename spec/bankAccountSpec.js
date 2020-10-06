3bf1210bfa0f4709c413209e9a0ad87d49d25505
Yasmin Underdown <yunderdown1@gmail.com>
2020-10-06 16:38:30 +0100

    ESLint installed and run

--- spec/bankAccountSpec.js
+++ spec/bankAccountSpec.js
@@ -1,66 +1,65 @@
+/* eslint-disable no-undef */
 'use strict'
 
-describe('BankAccount', function(){
+describe('BankAccount', function () {
+  var account
 
-    var account;
-    
-    beforeEach(function(){
-        account = new BankAccount();
-        account.date = new Date(2020, 9, 6);
-    });
+  beforeEach(function () {
+    account = new BankAccount()
+    account.date = new Date(2020, 9, 6)
+  })
 
-    it('prints blank statement with date credit debit and amount headers if no transactions have been made', function(){
-        expect(account.printStatement()).toEqual("date || credit || debit || balance")
-    });
+  it('prints blank statement with date credit debit and amount headers if no transactions have been made', function () {
+    expect(account.printStatement()).toEqual('date || credit || debit || balance')
+  })
 
-    it('adds amount to balance when deposit occurs and is shown on statement', function(){
-        account.deposit(1000);
-        expect(account.balance).toEqual(1000)
-        expect(account.transactions.length).toEqual(1)
-        expect(account.printStatement()).toContain("|| 1000.00 || || 1000.00")
-    });
+  it('adds amount to balance when deposit occurs and is shown on statement', function () {
+    account.deposit(1000)
+    expect(account.balance).toEqual(1000)
+    expect(account.transactions.length).toEqual(1)
+    expect(account.printStatement()).toContain('|| 1000.00 || || 1000.00')
+  })
 
-    it('subtracts amount from balance when withdrawal occurs and is shown on statement', function(){
-        account.balance = 1000;
-        account.withdraw(250);
-        expect(account.balance).toEqual(750)
-        expect(account.transactions.length).toEqual(1)
-        expect(account.printStatement()).toContain("|| || 250.00 || 750.00")
-    });
+  it('subtracts amount from balance when withdrawal occurs and is shown on statement', function () {
+    account.balance = 1000
+    account.withdraw(250)
+    expect(account.balance).toEqual(750)
+    expect(account.transactions.length).toEqual(1)
+    expect(account.printStatement()).toContain('|| || 250.00 || 750.00')
+  })
 
-    it('will not withdraw amount if it is less than balance', function() {
-        var withdraw = function() {
-            account.withdraw(2000);
-        }
-        expect(withdraw).toThrow("Insufficient funds available");
-        expect(account.balance).toEqual(0)
-        expect(account.transactions.length).toEqual(0)
-        expect(account.printStatement()).toEqual("date || credit || debit || balance")
-        
-    });
+  it('will not withdraw amount if it is less than balance', function () {
+    var withdraw = function () {
+      account.withdraw(2000)
+    }
+    expect(withdraw).toThrow('Insufficient funds available')
+    expect(account.balance).toEqual(0)
+    expect(account.transactions.length).toEqual(0)
+    expect(account.printStatement()).toEqual('date || credit || debit || balance')
+  })
 
-    it('statement shows date for deposit', function() {
-        account.deposit(1000);
-        expect(account.printStatement()).toContain(`6/10/2020 || 1000.00 || || 1000.00`)
-    });
+  it('statement shows date for deposit', function () {
+    account.deposit(1000)
+    expect(account.printStatement()).toContain('6/10/2020 || 1000.00 || || 1000.00')
+  })
 
-    it('statement shows date for withdrawal', function() {
-        account.balance = 1000;
-        account.withdraw(250);
-        expect(account.printStatement()).toContain(`6/10/2020 || || 250.00 || 750.00`)
-    });
+  it('statement shows date for withdrawal', function () {
+    account.balance = 1000
+    account.withdraw(250)
+    expect(account.printStatement()).toContain('6/10/2020 || || 250.00 || 750.00')
+  })
 
-    it('statement shows multiple transactions in reverse order', function(){
-        account.deposit(1000)
-        account.date = new Date(2020, 9, 7);
-        account.deposit(2000)
-        account.date = new Date(2020, 9, 8);
-        account.withdraw(500)
-        var expectedString = `date || credit || debit || balance
+  it('statement shows multiple transactions in reverse order', function () {
+    account.deposit(1000)
+    account.date = new Date(2020, 9, 7)
+    account.deposit(2000)
+    account.date = new Date(2020, 9, 8)
+    account.withdraw(500)
+    var expectedString = `date || credit || debit || balance
 8/10/2020 || || 500.00 || 2500.00
 7/10/2020 || 2000.00 || || 3000.00
 6/10/2020 || 1000.00 || || 1000.00`
-        console.log(expectedString)
-        expect(account.printStatement()).toEqual(expectedString)
-    });
-});
+    console.log(expectedString)
+    expect(account.printStatement()).toEqual(expectedString)
+  })
+})
--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,46 +1,44 @@
 'use strict'
 
 class BankAccount {
+  constructor () {
+    this.balance = 0
+    this.transactions = []
+    this.date = new Date()
+  }
 
-    constructor() {
-        this.balance = 0
-        this.transactions =  []
-        this.date = new Date()
+  deposit (amount) {
+    this.balance += amount
+    this.transactions.push(`${this.dateString()} || ${amount.toFixed(2)} || || ${this.balance.toFixed(2)}`)
+  }
+
+  withdraw (amount) {
+    if (amount > this.balance) {
+      throw 'Insufficient funds available'
+    } else {
+      this.balance -= amount
+      this.transactions.push(`${this.dateString()} || || ${amount.toFixed(2)} || ${this.balance.toFixed(2)}`)
     }
+  }
 
-    deposit(amount) {
-        this.balance += amount
-        this.transactions.push(`${this.dateString()} || ${amount.toFixed(2)} || || ${this.balance.toFixed(2)}`)
+  printStatement () {
+    console.log(this._statementHeaders() + this._statement())
+    return this._statementHeaders() + this._statement()
+  }
+
+  dateString () {
+    return this.date.getDate() + '/' + (this.date.getMonth() + 1) + '/' + this.date.getFullYear()
+  }
+
+  _statementHeaders () {
+    return 'date || credit || debit || balance'
+  }
+
+  _statement () {
+    var statement = ''
+    for (var i = this.transactions.length; i > 0; i--) {
+      statement += `\n${this.transactions[i - 1]}`
     }
-
-    withdraw(amount) {
-        if(amount > this.balance) {
-            throw "Insufficient funds available"
-        } else {
-            this.balance -= amount
-            this.transactions.push(`${this.dateString()} || || ${amount.toFixed(2)} || ${this.balance.toFixed(2)}`)
-        }
-    }
-
-    printStatement() {
-        console.log(this._statementHeaders() + this._statement())
-        return this._statementHeaders() + this._statement();
-    }
-
-    dateString() {
-        return this.date.getDate() + "/" + (this.date.getMonth() + 1) + "/" + this.date.getFullYear();
-    }
-
-    _statementHeaders() {
-        return "date || credit || debit || balance"
-    }
-
-    _statement() {
-        var statement = "";
-        for(var i=this.transactions.length; i>0; i--) {
-            statement += `\n${this.transactions[i-1]}`
-        }
-        return statement
-    }
-
+    return statement
+  }
 }
