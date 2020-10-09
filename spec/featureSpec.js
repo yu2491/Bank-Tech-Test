135f8adf2d9bcdde589358598718a4d8bbfbc64c
Yasmin Underdown <yunderdown1@gmail.com>
2020-10-09 16:52:46 +0100

    Date mocking changed, all tests passing

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -1,45 +1,44 @@
-// 'use strict'
+'use strict'
 
-// describe('Feature test', function(){
+describe('Feature test', function(){
 
-//     var BankAccount = require('../src/bankAccount.js');
-//     var account;
-//     var today;
+    var BankAccount = require('../src/bankAccount.js');
+    var account;
 
-//     beforeEach(function(){
-//         account = new BankAccount();
-//         account.balance = 1000
-//         var date = new Date()
-//         today = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
-//     });
+    beforeEach(function(){
+        account = new BankAccount();
+        account.balance = 1000
+        var dateDouble = new Date(2020, 9, 5);
+        jasmine.clock().mockDate(dateDouble);
+    });
 
-//     it('handles no transactions', function(){
-//         spyOn(console, 'log');
-//         account.printStatement();
-//         expect(console.log).toHaveBeenCalledWith("date || credit || debit || balance");
-//     });
+    it('handles no transactions', function(){
+        spyOn(console, 'log');
+        account.printStatement();
+        expect(console.log).toHaveBeenCalledWith("date || credit || debit || balance");
+    });
 
-//     it('handles 1 credit transaction', function () {
-//         account.deposit(1000)
-//         spyOn(console, 'log');
-//         account.printStatement()
-//         expect(console.log).toHaveBeenCalledWith(`date || credit || debit || balance\n${today} || 1000.00 || || 2000.00`);
-//     });
+    it('handles 1 credit transaction', function () {
+        account.deposit(1000)
+        spyOn(console, 'log');
+        account.printStatement()
+        expect(console.log).toHaveBeenCalledWith(`date || credit || debit || balance\n05/10/2020 || 1000.00 || || 2000.00`);
+    });
 
-//     it('handles 1 debit transaction', function () {
-//         account.withdraw(250);
-//         spyOn(console, 'log');
-//         account.printStatement();
-//         expect(console.log).toHaveBeenCalledWith(`date || credit || debit || balance\n${today} || || 250.00 || 750.00`);
-//     });
+    it('handles 1 debit transaction', function () {
+        account.withdraw(250);
+        spyOn(console, 'log');
+        account.printStatement();
+        expect(console.log).toHaveBeenCalledWith(`date || credit || debit || balance\n05/10/2020 || || 250.00 || 750.00`);
+    });
 
-//     it('statement shows multiple transactions in reverse order', function () {
-//         account.deposit(1000);
-//         account.deposit(2000);
-//         account.withdraw(500);
-//         var expectedString = `date || credit || debit || balance\n${today} || || 500.00 || 3500.00\n${today} || 2000.00 || || 4000.00\n${today} || 1000.00 || || 2000.00`
-//         spyOn(console, 'log');
-//         account.printStatement();
-//         expect(console.log).toHaveBeenCalledWith(expectedString)
-//     });
-// });
+    it('statement shows multiple transactions in reverse order', function () {
+        account.deposit(1000);
+        account.deposit(2000);
+        account.withdraw(500);
+        var expectedString = `date || credit || debit || balance\n05/10/2020 || || 500.00 || 3500.00\n05/10/2020 || 2000.00 || || 4000.00\n05/10/2020 || 1000.00 || || 2000.00`
+        spyOn(console, 'log');
+        account.printStatement();
+        expect(console.log).toHaveBeenCalledWith(expectedString)
+    });
+});
--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -4,13 +4,10 @@ describe('Transaction', function(){
 
     var Transaction = require('../src/transaction.js');
     var transaction;
-    var dateDouble;
     
     beforeEach(function(){
-        dateDouble = {};
-        dateDouble.getDate = function() { return 6; }
-        dateDouble.getMonth = function() { return 8; }
-        dateDouble.getFullYear = function() { return 2020 }
+        var dateDouble = new Date(2020, 8, 5);
+        jasmine.clock().mockDate(dateDouble);
         transaction = new Transaction('credit', 1000, dateDouble)
     });
 
@@ -19,7 +16,7 @@ describe('Transaction', function(){
     });
 
     it('can return its date', function(){
-        expect(transaction.getDate()).toEqual('06/09/2020')
+        expect(transaction.getDate()).toEqual('05/09/2020')
     });
 
     it('can return its amount', function(){
